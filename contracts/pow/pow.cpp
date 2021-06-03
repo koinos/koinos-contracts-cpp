3cfffd5be3621bcd19b1863bafcc2785e9276230
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-06-03 10:45:23 -0700

    Remove debug logging #4

--- contracts/pow/pow.cpp
+++ contracts/pow/pow.cpp
@@ -63,7 +63,6 @@ difficulty_metadata get_difficulty_meta()
 
 uint256_t get_and_update_difficulty( timestamp_type current_block_time )
 {
-   system::print( "Getting current difficulty\n" );
    auto diff_meta = get_difficulty_meta();
    auto old_difficulty = diff_meta.current_difficulty;
 
@@ -71,7 +70,6 @@ uint256_t get_and_update_difficulty( timestamp_type current_block_time )
    {
       if ( diff_meta.averaging_window >= BLOCK_AVERAGING_WINDOW )
       {
-         system::print( "Decaying block window time\n" );
          // Decay block window time
          diff_meta.block_window_time = diff_meta.block_window_time * (diff_meta.averaging_window - 1) / diff_meta.averaging_window;
       }
@@ -80,30 +78,31 @@ uint256_t get_and_update_difficulty( timestamp_type current_block_time )
          diff_meta.averaging_window++;
       }
 
-      system::print( "Adding current block interval\n" );
       diff_meta.block_window_time += current_block_time - diff_meta.last_block_time;
-
-      system::print( "current_block_time: " + std::to_string( current_block_time.t ) + ", last_block_time: " + std::to_string( diff_meta.last_block_time.t ) + ", block_window_time: " + std::to_string( diff_meta.block_window_time.t ) + '\n' );
 
       if ( current_block_time / UPDATE_DIFFICULTY_DELTA_MS > diff_meta.last_block_time / UPDATE_DIFFICULTY_DELTA_MS )
       {
-         system::print( "Updating difficulty for new minute\n" );
+         uint64_t average_block_interval = diff_meta.block_window_time / diff_meta.averaging_window;
 
-         system::print( "Calculating average block interval\n" );
-         int64_t average_block_interval = diff_meta.block_window_time / diff_meta.averaging_window;
+         if ( average_block_interval < TARGET_BLOCK_INTERVAL_MS )
+         {
+            auto block_time_diff = ( TARGET_BLOCK_INTERVAL_MS - average_block_interval );
 
-         system::print( "Calculating difference between observed and desired block interval\n" );
-         auto block_time_diff = ( TARGET_BLOCK_INTERVAL_MS - average_block_interval ) / 1440;
+            // This is a loss of precision, but it is a 256bit value, we should be fine
+            diff_meta.current_difficulty -= ( diff_meta.current_difficulty / ( TARGET_BLOCK_INTERVAL_MS * 60 ) ) * block_time_diff;
+         }
+         else if ( average_block_interval > TARGET_BLOCK_INTERVAL_MS )
+         {
+            auto block_time_diff = ( average_block_interval - TARGET_BLOCK_INTERVAL_MS );
 
-         system::print( "Calculating new difficulty\n" );
-         diff_meta.current_difficulty -= ( diff_meta.current_difficulty * block_time_diff ) / TARGET_BLOCK_INTERVAL_MS;
+            // This is a loss of precision, but it is a 256bit value, we should be fine
+            diff_meta.current_difficulty += ( diff_meta.current_difficulty / ( TARGET_BLOCK_INTERVAL_MS * 60 ) ) * block_time_diff;
+         }
       }
    }
 
-   system::print( "Setting block time\n" );
    diff_meta.last_block_time = current_block_time;
 
-   system::print( "Saving difficulty object\n" );
    system::db_put_object( contract_id, DIFFICULTY_METADATA_KEY, diff_meta );
 
    return old_difficulty;
@@ -120,50 +119,29 @@ int main()
       system::exit_contract( 0 );
    }
 
-   system::print( "getting contract args\n" );
    auto args = system::get_contract_args< chain::verify_block_signature_args >();
-   system::print( "unpacking pow signature data\n" );
    auto signature_data = pack::from_variable_blob< pow_signature_data >( args.signature_data );
-   system::print( "Nonce: " + signature_data.nonce.str() + '\n' );
-   system::print( "Digest: " + pack::from_variable_blob< uint256 >( pack::to_variable_blob( args.digest.digest ) ).str() + '\n' );
-   system::print( "Copying nonce to vb\n" );
    auto to_hash = pack::to_variable_blob( signature_data.nonce );
-   system::print( "Appending digest to vb\n" );
    to_hash.insert( to_hash.end(), args.digest.digest.begin(), args.digest.digest.end() );
 
-   system::print( "Hashing id + digest\n" );
    auto pow = pack::from_variable_blob< uint256_t >( system::hash( CRYPTO_SHA2_256_ID, to_hash ).digest );
 
    // Get/update difficulty from database
-   system::print( "Getting and updating difficulty target\n" );
    auto target = get_and_update_difficulty( system::get_head_block_time() );
-
-   system::print( "Difficulty target: " + target.str() + '\n' );
 
    if ( pow > target )
    {
-      system::print( "pow did not meet target\n" );
       system::set_contract_return( false );
       system::exit_contract( 0 );
    }
 
-   system::print( "Recovering block producer key\n" );
    // Recover address from signature
    auto producer = system::recover_public_key( pack::to_variable_blob( signature_data.recoverable_signature ), args.digest );
 
-   system::print( "Creating token class\n" );
    // Mint block reward to address
    auto koin_token = koinos::token( KOIN_CONTRACT );
 
-   system::print( "Minting block reward to producer\n" );
    auto success = koin_token.mint( producer, BLOCK_REWARD );
-
-   if ( !success )
-   {
-      system::print( "could not mint KOIN to producer address " + std::string( producer.data(), producer.size() ) + '\n' );
-   }
-
-   system::print( "great success!\n" );
    system::set_contract_return( success );
    system::exit_contract( 0 );
 
