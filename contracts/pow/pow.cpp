72d3044edb205f55c77649f311c6419e024a1965
Steve Gerbino <steve@gerbino.co>
2021-09-23 17:03:21 -0400

    #26: Partial conversion of PoW contract

--- contracts/pow/pow.cpp
+++ contracts/pow/pow.cpp
@@ -1,15 +1,19 @@
 #include <koinos/system/system_calls.hpp>
 #include <koinos/token.hpp>
 
+#include <boost/multiprecision/cpp_int.hpp>
+
 using namespace koinos;
 
 #define KOIN_CONTRACT uint160_t( "0x930f7a991f8787bd485b02684ffda52570439794" )
 #define BLOCK_REWARD  10000000000 // 100 KOIN
-#define TARGET_BLOCK_INTERVAL_MS 10000
+#define TARGET_BLOCK_INTERVAL_S 10
 #define DIFFICULTY_METADATA_KEY uint256_t( 0 )
 #define GET_DIFFICULTY_ENTRYPOINT 0x4a758831
 #define CRYPTO_SHA2_256_ID uint64_t(0x12)
 #define POW_END_DATE 1640995199000 // 2021-12-31T23:59:59Z
+
+using uint256_t = boost::multiprecision::uint256_t;
 
 uint256_t contract_id;
 
@@ -19,61 +23,30 @@ struct pow_signature_data
    fixed_blob< 65 > recoverable_signature;
 };
 
-KOINOS_REFLECT( pow_signature_data, (nonce)(recoverable_signature) )
 
-struct difficulty_metadata_v1
+struct difficulty_metadata
 {
    uint256_t      target = 0;
-   timestamp_type last_block_time = timestamp_type( 0 );
-   timestamp_type block_window_time = timestamp_type( 0 );
-   uint32_t       averaging_window = 0;
-};
-
-KOINOS_REFLECT( difficulty_metadata_v1,
-   (target)
-   (last_block_time)
-   (block_window_time)
-   (averaging_window)
-)
-
-struct difficulty_metadata_v2
-{
-   uint256_t      target = 0;
-   timestamp_type last_block_time = timestamp_type( 0 );
+   uint64_t       last_block_time = timestamp_type( 0 );
    uint256_t      difficulty = 0;
-   timestamp_type target_block_interval = timestamp_type( TARGET_BLOCK_INTERVAL_MS / 1000 );
+   uint64_t       target_block_interval = TARGET_BLOCK_INTERVAL_S;
 };
 
-KOINOS_REFLECT( difficulty_metadata_v2,
-   (target)
-   (last_block_time)
-   (difficulty)
-   (target_block_interval)
-)
 
-difficulty_metadata_v2 initialize_difficulty( const difficulty_metadata_v1& pow1_meta )
+void initialize_difficulty( difficulty_metadata diff_meta )
 {
-   difficulty_metadata_v2 diff_meta;
-   diff_meta.target = pow1_meta.target;
-   diff_meta.difficulty = std::numeric_limits< uint256_t >::max() / pow1_meta.target;
-   diff_meta.last_block_time = pow1_meta.last_block_time;
-
-   return diff_meta;
+   diff_meta.target = std::numeric_limits< uint256_t >::max() / (1 << 20),
+   diff_meta.last_block_time = system::get_head_block_time(),
+   diff_meta.difficulty = 1 << 20,
+   diff_meta.target_block_interval = TARGET_BLOCK_INTERVAL_S;
 }
 
-difficulty_metadata_v2 get_difficulty_meta()
+difficulty_metadata get_difficulty_meta()
 {
-   difficulty_metadata_v2 diff_meta;
-   auto diff_meta_vb = system::db_get_object( contract_id, DIFFICULTY_METADATA_KEY );
-
-   if ( diff_meta_vb.size() == sizeof( difficulty_metadata_v2 ) )
+   difficulty_metadata diff_meta;
+   if ( !system::get_object( contract_id, DIFFICULTY_METADATA_KEY, diff_meta ) )
    {
-      diff_meta = pack::from_variable_blob< difficulty_metadata_v2 >( diff_meta_vb );
-   }
-   else
-   {
-      auto pow1_meta = pack::from_variable_blob< difficulty_metadata_v1 >( diff_meta_vb );
-      diff_meta = initialize_difficulty( pow1_meta );
+      initialize_difficulty( diff_meta );
    }
 
    return diff_meta;
@@ -85,7 +58,7 @@ void update_difficulty( difficulty_metadata_v2& diff_meta, timestamp_type curren
    diff_meta.last_block_time = current_block_time;
    diff_meta.target = std::numeric_limits< uint256_t >::max() / diff_meta.difficulty;
 
-   system::db_put_object( contract_id, DIFFICULTY_METADATA_KEY, diff_meta );
+   system::put_object( contract_id, DIFFICULTY_METADATA_KEY, diff_meta );
 }
 
 int main()
@@ -107,14 +80,18 @@ int main()
       system::exit_contract( 0 );
    }
 
-   if ( system::get_caller().caller_privilege != chain::privilege::kernel_mode )
+   const auto [ caller, privilege ] = system::get_caller();
+   if ( privilege != chain::privilege::kernel_mode )
    {
       system::print( "pow contract must be called from kernel" );
       system::set_contract_return( false );
       system::exit_contract( 0 );
    }
 
-   auto args = system::get_contract_args< chain::verify_block_signature_args >();
+   auto args = system::get_contract_args();
+   koinos::read_buffer rdbuf( (uint8_t*)args.c_str(), args.size() );
+
+
    auto signature_data = pack::from_variable_blob< pow_signature_data >( args.signature_data );
    auto to_hash = pack::to_variable_blob( signature_data.nonce );
    to_hash.insert( to_hash.end(), args.digest.digest.begin(), args.digest.digest.end() );
